cb0af39e3d42d2ac46666ee6974f2d5607463b72
pv <pvacarizas@risingtide.ph>
2017-05-29 12:33:20 +0800

    fix paddle hit, add game start/end and ball serve

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //var game = new Phaser.Game(400, 300, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
 
-var MOV_SPEED = 550;
-var BALL_SPEED = 400;
+var MOV_SPEED = 400;
+var BALL_SPEED = 550;
 var gold =0;
 var red = 0;
 
@@ -17,28 +17,7 @@ function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //enemy
-    enemy = game.add.sprite(Math.ceil(game.world.centerX) - 32, 80, 'enemy');
-    enemy.anchor.setTo(0.5, 0.5);
-    game.physics.enable(enemy, Phaser.Physics.ARCADE);
-    enemy.body.collideWorldBounds = true;
-    enemy.body.checkCollision.up = false;
-    enemy.body.immovable = true;
-
-    //player
-    paddle = game.add.sprite(game.world.centerX - 32, game.world.height - 80, 'paddle');
-    paddle.anchor.setTo(0.5, 0.5);
-    game.physics.enable(paddle, Phaser.Physics.ARCADE);
-    paddle.body.collideWorldBounds = true;
-    paddle.body.checkCollision.down = false;
-    paddle.body.immovable = true;
-
-    //ball
-    star = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
-    game.physics.enable(star, Phaser.Physics.ARCADE);
-    star.body.velocity.setTo(BALL_SPEED, BALL_SPEED);
-    star.body.collideWorldBounds = true;
-    star.body.bounce.set(1);
+    gameSet();
 
     //pointer
     game.input.addPointer();
@@ -50,7 +29,7 @@ function create() {
 
 function update() {
     game.physics.arcade.collide(star, paddle, ballHitPaddle, null, this);
-    game.physics.arcade.collide(star, enemy);
+    game.physics.arcade.collide(star, enemy, ballHitPaddle, null, this);
     //game.physics.arcade.collide(enemy, paddle);
     
     // Right adjustments
@@ -77,10 +56,10 @@ function update() {
     }
 
     //pointer
-    if(star.body.y == 0) {
+    if(star.body.y == 0 && star.alive) {
         gold += 1;
         updatescore();
-    }else if (star.body.y == game.world.height - star.body.height) {
+    }else if (star.body.y == game.world.height - star.body.height && star.alive) {
         red += 1;
         updatescore();
     }
@@ -96,26 +75,28 @@ function render() {
 function ballHitPaddle (_ball, _paddle) {
 
     var diff = 0;
-    var ball_center = _ball.x+12;
-    var paddle_center = _paddle.x+32;
-
+    var ball_center = _ball.centerX;
+    var paddle_center = _paddle.centerX;
 
     if (ball_center < paddle_center)
     {
         //  Ball is on the left-hand side of the paddle
         diff = paddle_center - ball_center;
         _ball.body.velocity.x = (-10 * diff);
+        console.log('hit on LEFT -> '+_ball.body.velocity.x);
     }
     else if (ball_center > paddle_center)
     {
         //  Ball is on the right-hand side of the paddle
         diff = ball_center -paddle_center;
         _ball.body.velocity.x = (10 * diff);
+        console.log('hit on RIGHT -> '+_ball.body.velocity.x);
     }
     else
     {
         //  Ball is perfectly in the middle
         //  Add a little random X to stop it bouncing straight up!
+        console.log('hit on CENTER');
         _ball.body.velocity.x = 2 + Math.random() * 8;
     }
 
@@ -128,8 +109,54 @@ function followPoint(pointer) {
     }
 }
 
+function gameSet(){
+    //enemy
+    enemy = game.add.sprite(Math.ceil(game.world.centerX), 80, 'enemy');
+    enemy.anchor.setTo(0.5, 1);
+    game.physics.enable(enemy, Phaser.Physics.ARCADE);
+    enemy.body.collideWorldBounds = true;
+    enemy.body.checkCollision.up = false;
+    enemy.body.immovable = true;
+
+    //player
+    paddle = game.add.sprite(game.world.centerX, game.world.height - 80, 'paddle');
+    paddle.anchor.setTo(0.5, 0);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    paddle.body.collideWorldBounds = true;
+    paddle.body.checkCollision.down = false;
+    paddle.body.immovable = true;
+
+    //ball
+    star = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
+    star.anchor.setTo(0.5, 0);
+    game.physics.enable(star, Phaser.Physics.ARCADE);
+    //star.body.velocity.setTo(0, BALL_SPEED);
+    star.body.collideWorldBounds = true;
+    star.body.bounce.set(1);
+
+    //buttons
+    replayBtn = game.add.text(game.world.centerX, game.world.centerY, "start", { font: "65px Arial", fill: "#ffffff", align: "center" });
+    replayBtn.inputEnabled = true;
+    replayBtn.events.onInputUp.add(startGame, this);
+    star.anchor.setTo(0.5, 0);
+}
+
+function startGame(){
+    replayBtn.kill();
+    taa = game.time.events.add(Phaser.Timer.SECOND * 1, serveBall, this);
+    console.log("aaaaaa");
+}
+
+function serveBall(){
+    star.body.velocity.setTo(0, BALL_SPEED);
+}
+
 function updatescore(){
-    //star.kill();
+    star.kill();
+    enemy.kill();
+    paddle.kill();
     //score += 10;
     sb.text = "Gold: "+gold+"\n Red: "+red;
+    gameSet();
 }
+
